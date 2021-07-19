90bbca707631abcec652598996c5ab6ffedd54a7
Guilherme Souza <gms4@cin.ufpe.br>
2021-07-19 18:53:15 -0300

    sign extend

---
+++ parts_made/sign_extend.v
@@ -0,0 +1,11 @@
+module sign_extend(
+
+    input  wire [15:0] data_in,
+    output wire [31:0] data_out
+
+);
+
+     //se o bit de sinal for 1, extenderemos só com 1s, caso contrário, 0s
+    assign data_out = (data_in[15]) ? {{16{1'b1}}, data_in} : {{16{1'b0}}, data_in};
+
+endmodule
