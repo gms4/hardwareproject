92c0035ab83f6f386747fe79fe90181311ea8eda
Guilherme Souza <gms4@cin.ufpe.br>
2021-07-28 20:20:30 -0300

    the box 2 and cpu

---
+++ parts_made/the_box2.v
@@ -0,0 +1,14 @@
+module the_box2 (
+
+    input wire  [4:0] instr_input1,
+    input wire  [4:0] instr_input2,
+    input wire  [15:0] instr_input3,
+    output wire [25:0] data_out
+
+); 
+
+    //concatenando
+    assign data_out = {instr_input1, instr_input2, instr_input3};
+
+
+endmodule
