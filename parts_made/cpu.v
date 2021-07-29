caa080eefe65d840c22c24ee3fe4885ebe7be483
Guilherme Souza <gms4@cin.ufpe.br>
2021-07-29 01:56:57 -0300

    more cpu

--- parts_made/cpu.v
+++ parts_made/cpu.v
@@ -11,26 +11,35 @@ module cpu (
     wire        MEM_w;
     wire        EPC_load;
     wire        IR_load;
-
-    wire        muxWR;
-
+    wire        high_load; 
+    wire        low_load;
     wire [2:0]  mux_address_selector;
     wire        mux_wd_MEM_selector;
     wire        address_RG_load;
     wire        mdr_load;
     wire        store_size_selector;
+    wire [1:0]  mux_wr_reg_selector;
+    wire [2:0]  mux_wd_reg_selector;
+    wire        reg_wr;
+    wire        mux_high_selector;
+    wire        mux_low_selector;
+    wire        mux_extend_selector;
+    wire        gt;
+    wire        zero;
+    wire        isBEQ;
+    wire        isBNE;
+    wire        isBLE;
+    wire        isBGT;
 
 //data wires
 
-    wire [31:0] ALUout_to_PC;
+    wire [31:0] ALUout;
     wire [31:0] PC_out;
     wire [31:0] MEM_out;
     wire [5:0]  OPCODE;
     wire [4:0]  RS;
     wire [4:0]  RT;
     wire [15:0] IMMEDIATE;
-    wire [4:0]  WR_in;
-    wire [31:0] ALUout_to_muxaddress;
     wire [31:0] mux_address_out;
     wire [31:0] store_size_out;
     wire [31:0] B_out;
@@ -41,13 +50,29 @@ module cpu (
     wire [15:0] load_size_to_mux_extend;
     wire [31:0] load_size_to_mux_WD_reg;
     wire [25:0] the_box2_out;
+    wire [4:0]  mux_wr_reg_out;
+    wire [31:0] low_out;
+    wire [31:0] high_out;
+    wire [31:0] RegDesloc_out;
+    wire [31:0] sign_extend1_32_out;
+    wire [31:0] mux_wd_reg_out;
+    wire [31:0] mux_high_out;
+    wire [31:0] mux_low_out;
+    wire [31:0] read_data1_out;
+    wire [31:0] read_data2_out;
+    wire [31:0] mult_high_out;
+    wire [31:0] div_high_out;
+    wire [31:0] mult_low_out;
+    wire [31:0] div_low_out;
+    wire [15:0] mux_extend_out;
+
 
     Registrador PC_(
 
          clk,
          reset,
          PC_load,
-         ALUout_to_PC,
+         ALUout,
          PC_out
 
     );
@@ -56,7 +81,7 @@ module cpu (
 
          mux_address_selector,
          PC_out,
-         ALUout_to_muxaddress,
+         ALUout,
          mux_address_out
 
     );
@@ -154,16 +179,99 @@ module cpu (
     );
 
     mux_WR_Registers mux_wr_reg_(
-
        
-       
-    );
-
-    mux_WR_Registers muxWR_(
-       
-       muxWR, //selector
+       mux_wr_reg_selector,
        RT,
-       IMMEDIATE[15:11],
-       WR_in,
-       
+       IMMEDIATE,
+       mux_wr_reg_out
+
     );
+
+    mux_WD_Registers mux_wd_reg_(
+
+       mux_wd_reg_selector,
+       load_size_to_mux_WD_reg,
+       ALUout,
+       Low_out,
+       High_out,
+       RegDesloc_out,
+       sign_extend1_32_out,
+       mux_wd_reg_out
+
+    );
+
+    Registrador high_(
+
+        clk,
+        reset,
+        high_load,
+        mux_high_out,
+        high_out
+
+    );
+
+    Registrador low_(
+
+        clk,
+        reset,
+        low_load,
+        mux_low_out,
+        low_out
+
+    );
+
+    Banco_reg registers_(
+
+        clk,
+        reset,
+        reg_wr,
+        RT,
+        RS,
+        mux_wr_reg_out,
+        mux_wd_reg_out,
+        read_data1_out,
+        read_data2_out
+
+    );
+
+    mux_High mux_high_(
+        
+        mux_high_selector,
+        mult_high_out,
+        div_high_out,
+        mux_high_out
+
+    );
+
+    mux_Low mux_low_(
+
+        mux_low_selector,
+        mult_low_out,
+        div_low_out,
+        mux_low_out
+
+    );
+
+    mux_Extend mux_extend_(
+
+        mux_extend_selector,
+        load_size_to_mux_extend,
+        IMMEDIATE,
+        mux_extend_out
+
+    );
+
+    controller controller_(
+
+       gt,
+       zero,
+       pc_write,
+       isBEQ,
+       isBNE,
+       isBLE,
+       isBGT,
+       PC_load
+
+    );
+
+    
