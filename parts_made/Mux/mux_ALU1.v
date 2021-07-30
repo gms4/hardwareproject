02af7961caafbb45cb8a3fce22c4f9d8c9795866
Uiusu <jwcfj@cin.ufpe.br>
2021-07-30 18:45:47 -0300

    Coment add

--- parts_made/Mux/mux_ALU1.v
+++ parts_made/Mux/mux_ALU1.v
@@ -17,6 +17,10 @@ module mux_ALU1(
 
 wire [31:0] out1;
 
+//00 data_0 ok
+//01 data_1 ok
+//10 32'd0 ok
+
 assign out1 = (selector[0]) ? data_1 : data_0;
 assign data_out = (selector[1]) ? 32'd0 : out1;
 
