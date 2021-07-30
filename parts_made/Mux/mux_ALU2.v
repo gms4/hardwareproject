75529b996977da4a289560da3b742d0d1ac0cb0b
Uiusu <jwcfj@cin.ufpe.br>
2021-07-30 18:39:44 -0300

    Update mux_ALU2
    Lógica do mux_ALU2 atualizada

--- parts_made/Mux/mux_ALU2.v
+++ parts_made/Mux/mux_ALU2.v
@@ -18,10 +18,22 @@ data_3 --|--out2-------|/
 */
 
 wire [31:0] out1, out2;
+/*
+if(selector[0]==1)
+    return 32'd4;
+else
+    data_0
+*/
+
+
+//00 data0 ok
+//01 32'd4  ok
+//10 data2 ok
+//11 data3 ok
 
 assign out1 = (selector[0]) ? 32'd4 : data_0;
-assign out2 = (selector[0]) ? data_2 : data_3;
-assign data_out = (selector[1]) ? out1 : out2;
+assign out2 = (selector[0]) ? data_3 : data_2;
+assign data_out = (selector[1]) ? out2 : out1 ;
 
 
 endmodule
